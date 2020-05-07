3ea75852c57994a8e2901abb742f45045fa10e75
fabrizzioCar <70674383@ucsm.edu.pe>
2020-05-06 20:34:46 -0500

    Register activity falta implementar funcionalidad, accesso a db, etc

--- app/src/main/java/com/example/taypoyapp/Login.java
+++ app/src/main/java/com/example/taypoyapp/Login.java
@@ -2,13 +2,26 @@ package com.example.taypoyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class Login extends AppCompatActivity {
+    Button btn_go_register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btn_go_register=findViewById(R.id.btn_go_register);
+        btn_go_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
     }
+
 }
--- app/src/main/java/com/example/taypoyapp/Register.java
+++ app/src/main/java/com/example/taypoyapp/Register.java
@@ -2,13 +2,23 @@ package com.example.taypoyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
 public class Register extends AppCompatActivity {
 
+    Button btn_go_login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        btn_go_login=findViewById(R.id.btn_go_login);
+        btn_go_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Register.this, Login.class));
+            }
+        });
     }
 }
