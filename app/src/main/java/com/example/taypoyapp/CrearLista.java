7e627bbd20a3b572e4f4988c92a6120f3ca2dc4c
Parzibyte360 <brunosantospamo@gmail.com>
2020-06-07 18:22:44 -0500

    Creacion de SplashScreen

---
+++ app/src/main/java/com/example/taypoyapp/CrearLista.java
@@ -0,0 +1,14 @@
+package com.example.taypoyapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearLista extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_lista);
+    }
+}
