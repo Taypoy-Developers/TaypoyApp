9912a248b7186d9648b50bb0eb585ef942e86f5a
Moriarty-Rzer <64698169+Moriarty-Rzer@users.noreply.github.com>
2020-06-07 19:23:04 -0500

    Mensajes

---
+++ app/src/main/java/com/example/taypoyapp/MyService.java
@@ -0,0 +1,23 @@
+package com.example.taypoyapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+public class MyService extends FirebaseMessagingService {
+    private static final String TAG = "0";
+    public MyService() {
+    }
+
+
+    public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.d(TAG, "From: " + remoteMessage.getFrom());
+    }
+}
