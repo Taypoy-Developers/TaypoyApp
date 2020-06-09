fc471fdec54bc518285c3cbe2085b96ff81a776d
Parzibyte360 <brunosantospamo@gmail.com>
2020-06-09 12:49:28 -0500

    Modificacion del activity CrearLista

--- app/src/main/java/com/example/taypoyapp/CrearLista.java
+++ app/src/main/java/com/example/taypoyapp/CrearLista.java
@@ -4,9 +4,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class CrearLista extends AppCompatActivity {
+
+    public static String[] CARNES ={
+            "Pollo",
+            "Carne de Res",
+            "Pescado",
+            "Carne de Cerdo"
+    };
+    public static String[] FRUTAS ={
+            "Manzana",
+            "Naranja",
+            "Mandarina",
+            "Platano",
+            "Pera",
+            "Papaya",
+            "Mango",
+            "Fresa"
+    };
+    public static String[] VERDURAS ={
+            "Cebolla",
+            "Papa",
+            "Zanahoria",
+            "Lechuga",
+            "Apio",
+            "Tomate",
+            "Espinaca"
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +47,37 @@ public class CrearLista extends AppCompatActivity {
 
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(username);
+
+        LinearLayout seccion_carnes = (LinearLayout) findViewById(R.id.seccion_carnes);
+
+        for (String carne : CARNES) {
+            CheckBox opcion1 = new CheckBox(this);
+            opcion1.setText(carne);
+            opcion1.setLayoutParams(
+                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            seccion_carnes.addView(opcion1);
+        }
+
+        LinearLayout seccion_frutas = (LinearLayout) findViewById(R.id.seccion_frutas);
+
+        for (String fruta : FRUTAS) {
+            CheckBox opcion2 = new CheckBox(this);
+            opcion2.setText(fruta);
+            opcion2.setLayoutParams(
+                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            seccion_frutas.addView(opcion2);
+        }
+
+        LinearLayout seccion_verduras = (LinearLayout) findViewById(R.id.seccion_verduras);
+
+        for (String verdura : VERDURAS) {
+            CheckBox opcion3 = new CheckBox(this);
+            opcion3.setText(verdura);
+            opcion3.setLayoutParams(
+                    new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+            seccion_verduras.addView(opcion3);
+        }
+
+
     }
 }
